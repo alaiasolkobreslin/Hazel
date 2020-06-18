813b849552ba55a0ae7fc5138bc8a3a078ab893b
Malsirhc <cl2424@cornell.edu>
2020-06-18 03:15:31 -0700

    Started on ast

---
+++ src/ast.ml
@@ -0,0 +1,47 @@
+type tuple = Placeholder
+type expr = 
+  | Unit 
+  | Int of int 
+  | Bool of bool 
+  | Var of string 
+  | Tuple of tuple
+  | IfThen of (expr * expr * expr)
+  | Let of (string * expr * expr)
+  | LetRec of (string * expr * expr)
+  | And of (string * expr * expr)
+  | BeginEnd of expr
+  | MatchWithWhen of (expr * (expr * expr * pattern) list) (*extra expr for when *)
+  | Function of (string list * expr)
+  | Application of (expr * expr)
+  | Assignment of (string * expr)
+  | Ref of expr
+  | Deref of string
+  | Binop of (bop * expr * expr)
+  | Unaop of (unop * expr)
+  | Constraint of (string * expr)
+  | Constructor of (string * types)
+and pattern = Placeholder2
+and bop = 
+  | Plus 
+  | Minus 
+  | Mult 
+  | Cons 
+  | Seq 
+  | GT 
+  | LT 
+  | EQ 
+  | NEQ 
+  | PEQ 
+  | PNEQ 
+  | Mod 
+  | BoolAnd
+  | BoolOr
+and unop = Not | Neg 
+and types = 
+  | Boolean 
+  | Integer 
+  | Product of (types * types) 
+  | Sum of (types * types)
+  | List of types
+  | Terminal
+  | Reference of types
