c2184ff97f1e39a22f339901708cbf28e24611e5
Malsirhc <cl2424@cornell.edu>
2020-07-17 17:59:55 -0700

    Fixed some typecheck bugs

--- src/typecheck.ml
+++ src/typecheck.ml
@@ -21,48 +21,62 @@ let make_constr_lst prog =
   match prog with
   |_,opn_list,typ_list,_ -> make_constr_lst_helper typ_list []
 
-(* let rec typecheck_expr exp constr environ = 
-   match exp with
-   |(parsed, Unit) -> ({typed_pos = parsed.parsed_pos; ttype = TUnit; env = environ}, Unit)
-   |(parsed, Nil) -> ({typed_pos = parsed.parsed_pos; ttype = TCons (TVar "'a"); env = environ}, Nil)
-   |(parsed, Bool b) -> ({typed_pos = parsed.parsed_pos; ttype = TBool; env = environ}, Bool b)
-   |(parsed, String str) -> ({typed_pos = parsed.parsed_pos; ttype = TString; env = environ}, String str)
-   |(parsed, Char c) -> ({typed_pos = parsed.parsed_pos; ttype = TChar; env = environ}, Char c)
-   |(parsed, Var x) -> ({typed_pos = parsed.parsed_pos; ttype = (List.assoc x environ); env = environ}, Var x)
-   |(parsed, Tuple lst) -> ({typed_pos = parsed.parsed_pos; ttype = TProd (List.map (fun x -> typecheck_expr x constr environ)); env = environ}, Tuple lst)
-   |_ -> failwith "typecheck error" *)
+let rec typecheck_expr exp constr environ = 
+  match exp with
+  |(parsed, Unit) -> 
+    ({typed_pos = parsed.parsed_pos; ttype = TUnit; env = environ}, Unit)
+  |(parsed, Nil) -> 
+    ({typed_pos = parsed.parsed_pos; ttype = TCons (TVar "'a"); env = environ}, Nil)
+  |(parsed, Bool b) -> 
+    ({typed_pos = parsed.parsed_pos; ttype = TBool; env = environ}, Bool b)
+  |(parsed, String str) -> 
+    ({typed_pos = parsed.parsed_pos; ttype = TString; env = environ}, String str)
+  |(parsed, Char c) -> 
+    ({typed_pos = parsed.parsed_pos; ttype = TChar; env = environ}, Char c)
+  |(parsed, Var x) -> 
+    ({typed_pos = parsed.parsed_pos; ttype = (List.assoc x environ); env = environ}, Var x)
+  |(parsed, Tuple lst) -> 
+    ({typed_pos = parsed.parsed_pos; ttype = TProd (List.map (fun x -> (fst (typecheck_expr x constr environ)).ttype) lst); env = environ}, Tuple lst)
+  |(parsed, IfThen (b, e1, e2)) -> 
+    let e1_typ = retrieve_type e1 constr environ in
+    let e2_typ = retrieve_type e1 constr environ in 
+    if (retrieve_type b constr environ = TBool) && e1_typ = e2_typ 
+    then ({typed_pos = parsed.parsed_pos; ttype = e1_typ; env = environ}, IfThen(b, e1, e2)) else failwith "typecheck error"
+  |_ -> failwith "typecheck error"
+
+and retrieve_type expr constr env = (fst (typecheck_expr expr constr env)).ttype
 
 
 (*could neaten this up by just passing in the type instead of the expression *)
-let rec update_environ pat exp constr environ = 
-  let expr_typ = typecheck_expr exp constr environ in
+and update_environ pat exp constr environ = 
+  let expr_typ = (fst (typecheck_expr exp constr environ)).ttype in
   begin
-    match pat, expr_typ with
-    |(_,PUnit),TUnit-> []
-    |(_,PBool b),TBool -> []
-    |(_,PInt i),TInt -> []
-    |(_,PString s),TString -> []
-    |(_,PVar x),t -> [(x, t)]
-    |(_,(PTup l1)),(TSum l2) -> failwith "aaargh I really don't want to do this one"
+    match (snd pat), expr_typ with
+    |PUnit,TUnit-> []
+    |PBool b,TBool -> []
+    |PInt i,TInt -> []
+    |PString s,TString -> []
+    |PVar x,t -> [(x, t)]
+    |PTup l1,(TSum l2) -> failwith "aaargh I really don't want to do this one"
     |_ -> failwith "unimplemented"
   end
 
-and typecheck_expr exp constr environ = 
-  match exp with
-  |(parsed, Unit) -> TUnit
-  |(parsed, Nil) -> TCons (TVar "'a")
-  |(parsed, Bool b) -> TBool
-  |(parsed, String str) -> TString
-  |(parsed, Char c) -> TChar
-  |(parsed, Var x) -> (List.assoc x environ)
-  |(parsed, Tuple lst) -> TProd (List.map (fun x -> typecheck_expr x constr environ) lst)
-  |(parsed, IfThen (b, e1, e2)) -> 
+(* and typecheck_expr exp constr environ = 
+   match snd exp with
+   |(Unit) -> TUnit
+   |(Nil) -> TCons (TVar "'a")
+   |(Bool b) -> TBool
+   |(String str) -> TString
+   |(Char c) -> TChar
+   |(Var x) -> (List.assoc x environ)
+   |(Tuple lst) -> TProd (List.map (fun x -> typecheck_expr x constr environ) lst)
+   |(IfThen (b, e1, e2)) -> 
     let e1_typ = typecheck_expr e1 constr environ in
     let e2_typ = typecheck_expr e2 constr environ in 
     if (typecheck_expr b constr environ = TBool) && e1_typ = e2_typ 
     then e1_typ else failwith "typecheck error"
-  |(parsed, Let (pat, e1, e2)) -> typecheck_expr e2 constr ((update_environ pat e1 constr environ)@environ)
-  |_ -> failwith "typecheck error"
+   |(Let (pat, e1, e2)) -> typecheck_expr e2 constr ((update_environ pat e1 constr environ)@environ)
+   |_ -> failwith "typecheck error" *)
 
 (* and 'a pattern = 
    | PUnit
