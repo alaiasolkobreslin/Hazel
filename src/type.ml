e06cea49a0128246e1cc4eb13aea603dc15bb6f3
Christopher Lam <mals@Christophers-MacBook-Pro-5.local>
2022-08-09 12:29:13 -0700

    started labeling for unification

--- src/type.ml
+++ src/type.ml
@@ -4,9 +4,12 @@ exception TypingError of (Lexing.position * string)
 
 type id = string
 (*type typ = Var of id | Int | Bool | Fun of typ * typ | Subst of typ ref*)
+type checked = Resolved | Unresolved of types
 
 type schema = id list * types
 type env = (id * schema) list
+type constructor = string
+type constructors = (constructor * (types option * string)) list 
 
 let lookup env key =
   match List.find_opt (fun (k, _) -> k = key) env with
@@ -14,6 +17,7 @@ let lookup env key =
   | None -> None
 
 (* collapse all Subst's except Vars *)
+
 let rec resolve (t : types) : types =
   match t with
   | (TInt | TBool | TVar _ | Subst {contents = TVar _}) -> t
@@ -30,6 +34,47 @@ let rec unify (t1 : types) (t2 :types) : unit =
   | ((Subst ({contents = TVar a} as r), t) | (t, Subst ({contents = TVar a} as r))) -> r := t
   | _ -> failwith "cannot unify"
 
+let fresh_var = failwith "unimplemented"
+
+let rec label_ast (expr) (var_env : (id * types) list) (cons : constructors) : types expr_ann = 
+  let (pos, e) = expr in
+  match e with
+  | Unit -> (TUnit, Unit)
+  | Nil -> failwith "unimplemented"
+  | Int n -> (TInt, Int n)
+  | Bool b -> (TBool, Bool b)
+  | String s -> (TString, String s)
+  | Char c -> (TChar, Char c)
+  | Var id ->
+    begin
+      match lookup var_env id with
+      | Some typ -> (typ, Var id)
+      | None -> failwith "Variable referenced before defined"
+    end
+  | Tuple [e1; e2] -> (TPlaceholder fresh_var, Tuple [label_ast e1 var_env cons; label_ast e2 var_env cons])
+  | IfThen (b, e1, e2) -> (TPlaceholder fresh_var, IfThen (label_ast b var_env cons, label_ast e1 var_env cons, label_ast e2 var_env cons))
+  | Let (pat, e1, e2) -> 
+    let (_, pat') = pat in
+    begin
+      match pat' with
+      | PUnit -> TPlaceholder fresh_var, Let((TUnit, PUnit), label_ast e1 var_env cons, label_ast e2 var_env cons)
+      | PWild -> TPlaceholder fresh_var, Let((TPlaceholder fresh_var, PWild), label_ast e1 var_env cons, label_ast e2 var_env cons)
+      | PBool b -> TPlaceholder fresh_var, Let((TBool, PBool b), label_ast e1 var_env cons, label_ast e2 var_env cons)
+      | PInt n -> TPlaceholder fresh_var, Let((TInt, PInt n), label_ast e1 var_env cons, label_ast e2 var_env cons)
+      | PString s -> TPlaceholder fresh_var, Let((TString, PString s), label_ast e1 var_env cons, label_ast e2 var_env cons)
+      | PVar s -> 
+        let name = TPlaceholder fresh_var in
+        let new_env = (s, name)::(List.remove_assoc s var_env) in
+        TPlaceholder fresh_var, Let((name, PVar s), label_ast e1 var_env cons, label_ast e2 new_env cons)
+      | PTup lst -> failwith "Honestly it's probably better to move this to a helper, but I'm already started"
+      | PSum (cons, pat) -> failwith "Again, go to helper"
+      | PNil -> TPlaceholder fresh_var, Let((TPlaceholder fresh_var, PNil), label_ast e1 var_env cons, label_ast e2 var_env cons)
+      | PCons (pat1, pat2) -> failwith "again, helper"
+    end
+  | _ -> failwith "unimplemented"
+
+and label_pats pat var_env cons = 
+failwith "unimplemented"
 let type_expr expr var_env typ_env =
   let (pos, e) = expr in
   match e with
@@ -43,7 +88,7 @@ let type_expr expr var_env typ_env =
     begin
       match lookup var_env id with
       | Some typ -> typ
-      | None -> raise (TypingError (pos, "unbound variable " ^ id))
+      | None -> failwith "Need fresh variable using TyVarVar here"
     end
   | _ -> failwith "unimplemented"
 
