ba6c21cf974fdae71d5a09ca2dd22c4f90984e8f
Alaia Solko-Breslin <ajs644@cornell.edu>
2022-09-02 09:36:37 -0400

    actually pushing typecheck :)

--- src/typecheck.ml
+++ src/typecheck.ml
@@ -1,15 +1,16 @@
 open Ast
 open Refinement
+open Type
 
 type constructor = string
 type constructors = (constructor * (types option * string)) list
 type env = (string * types) list
 
-let rec interleave_list acc x y =
+let rec interleave_list prsed acc x y =
   match (x, y) with
-  | h :: t, h2 :: t2 -> interleave_list ((h, h2) :: acc) t t2
+  | h :: t, h2 :: t2 -> interleave_list prsed ((h, h2) :: acc) t t2
   | [], [] -> List.rev acc
-  | _ -> failwith "Lists not of same length"
+  | _ -> raise (TypingError (prsed.parsed_pos, "Incompatible tuple lengths"))
 
 let rec retrieve_constructors lst acc str =
   match lst with
@@ -32,6 +33,7 @@ let make_constr_lst prog =
 let rec typecheck_expr exp constr environ : typed expr_ann =
   match exp with
   | parsed, Unit -> ({ typed_pos = parsed.parsed_pos; ttype = TUnit }, Unit)
+  | parsed, Int i -> ({ typed_pos = parsed.parsed_pos; ttype = TInt }, Int i)
   | parsed, Nil ->
       ({ typed_pos = parsed.parsed_pos; ttype = TCons (TVar "'a") }, Nil)
   | parsed, Bool b -> ({ typed_pos = parsed.parsed_pos; ttype = TBool }, Bool b)
@@ -58,7 +60,11 @@ let rec typecheck_expr exp constr environ : typed expr_ann =
       then
         ( { typed_pos = parsed.parsed_pos; ttype = (fst e1_typ).ttype },
           IfThen (b_typ, e1_typ, e2_typ) )
-      else failwith "typecheck error"
+      else
+        raise
+          (TypingError
+             ( parsed.parsed_pos,
+               "first argument not bool or incompatible branch types" ))
   | parsed, Let (pat, e1, e2) ->
       let new_pat = parsed_to_typed_pat pat in
       let e1_typed = typecheck_expr e1 constr environ in
@@ -72,13 +78,15 @@ let rec typecheck_expr exp constr environ : typed expr_ann =
   | parsed, MatchWithWhen (init, lst) -> (
       let init_typed = typecheck_expr init constr environ in
       let typed_cases =
-        p_match_helper (fst init_typed).ttype lst [] constr environ
+        p_match_helper parsed (fst init_typed).ttype lst [] constr environ
       in
       match typed_cases with
       | (e, o, p) :: t ->
           ( { typed_pos = parsed.parsed_pos; ttype = (fst e).ttype },
             MatchWithWhen (init_typed, typed_cases) )
-      | _ -> failwith "typecheck error")
+      | _ ->
+          raise (TypingError (parsed.parsed_pos, "match with when empty cases"))
+      )
   | parsed, Fun (pat, e) -> failwith "need unification"
   | parsed, App (f, e) -> (
       let f_typed = typecheck_expr f constr environ in
@@ -87,19 +95,22 @@ let rec typecheck_expr exp constr environ : typed expr_ann =
       | TFun (arg_t, e_t), t when arg_t = t ->
           ( { typed_pos = parsed.parsed_pos; ttype = e_t },
             App (f_typed, e_typed) )
-      | _ -> failwith "argument type does not match function")
+      | _ ->
+          raise
+            (TypingError
+               (parsed.parsed_pos, "argument type does not match function")))
   | parsed, Binop (b, e1, e2) ->
       typecheck_bop (parsed, Binop (b, e1, e2)) constr environ
-  | parsed, Unaop (u, e) -> typecheck_unop e constr environ
-  | _ -> failwith "typecheck error"
+  | parsed, Unaop (u, e) -> typecheck_unop exp constr environ
+  | _ -> failwith "typecheck_expr unimplemented"
 
-and p_match_helper typ lst acc constr env =
+and p_match_helper parsed typ lst acc constr env =
   match (lst, acc) with
   | (exp, None, pat) :: t, [] ->
       let new_pat = parsed_to_typed_pat pat in
       let new_environ = update_environ pat typ constr env in
       let typed_exp = typecheck_expr exp constr new_environ in
-      p_match_helper typ t [ (typed_exp, None, new_pat) ] constr env
+      p_match_helper parsed typ t [ (typed_exp, None, new_pat) ] constr env
   | (exp, Some b, pat) :: t, (e, o, p) :: t2 ->
       let new_pat = parsed_to_typed_pat pat in
       let new_environ = update_environ pat typ constr env in
@@ -107,12 +118,15 @@ and p_match_helper typ lst acc constr env =
       let typed_b = typecheck_expr b constr new_environ in
       if (fst e).ttype = (fst typed_exp).ttype && (fst typed_b).ttype = TBool
       then
-        p_match_helper typ t
+        p_match_helper parsed typ t
           ((typed_exp, Some typed_b, new_pat) :: acc)
           constr env
-      else failwith "typecheck error"
+      else
+        raise
+          (TypingError
+             ((fst exp).parsed_pos, "incompatible types in matchwithwhen"))
   | [], _ -> List.rev acc
-  | _ -> failwith "typecheck error"
+  | _ -> raise (TypingError (parsed.parsed_pos, "wrong number of branches"))
 
 (*could neaten this up by just passing in the type instead of the expression *)
 and update_environ pat (expr_typ : Ast.types) constr environ =
@@ -123,7 +137,7 @@ and update_environ pat (expr_typ : Ast.types) constr environ =
   | PString s, TString -> []
   | PVar x, t -> [ (x, t) ]
   | PTup l1, TProd l2 ->
-      let interleaved = interleave_list [] l1 l2 in
+      let interleaved = interleave_list (fst pat) [] l1 l2 in
       List.map (fun (x, y) -> update_environ x y constr environ) interleaved
       |> List.flatten
   | PSum (id, pat'), TSum x -> failwith "save for later"
@@ -139,42 +153,57 @@ and typecheck_bop exp constr environ =
     match exp with
     | _, Binop (op, e, e') ->
         (op, typecheck_expr e constr environ, typecheck_expr e' constr environ)
-    | _ -> failwith "Typecheck error"
+    | _ -> raise (TypingError (parsed.parsed_pos, "Expected binary operator"))
   in
   match bop with
   | Plus | Minus | Mult | Div | Mod | HMult ->
       if (fst e1).ttype = TInt && (fst e2).ttype = TInt then
         ({ typed_pos = parsed.parsed_pos; ttype = TInt }, Binop (bop, e1, e2))
-      else failwith "typecheck error"
+      else
+        raise
+          (TypingError
+             (parsed.parsed_pos, "Binary operator expects two integers"))
   | GT | GEQ | LT | LEQ ->
       if (fst e1).ttype = TInt && (fst e2).ttype = TInt then
         ({ typed_pos = parsed.parsed_pos; ttype = TBool }, Binop (bop, e1, e2))
-      else failwith "typecheck error"
+      else
+        raise
+          (TypingError
+             (parsed.parsed_pos, "Binary operator expects two integers"))
   | ConsBop -> (
       match ((fst e1).ttype, (fst e2).ttype) with
       | a, TCons b when a = b ->
           ( { typed_pos = parsed.parsed_pos; ttype = TCons b },
             Binop (bop, e1, e2) )
-      | _ -> failwith "Inconsistant types in list")
+      | _ ->
+          raise (TypingError (parsed.parsed_pos, "Inconsistant types in list")))
   | Seq ->
       if (fst e1).ttype = TUnit then
         ( { typed_pos = parsed.parsed_pos; ttype = (fst e2).ttype },
           Binop (bop, e1, e2) )
-      else failwith "typecheck error"
+      else raise (TypingError (parsed.parsed_pos, "Sequence expects unit type"))
   | EQ | NEQ ->
       if (fst e1).ttype = (fst e2).ttype then
         ( { typed_pos = parsed.parsed_pos; ttype = (fst e1).ttype },
           Binop (bop, e1, e2) )
-      else failwith "typecheck error"
+      else
+        raise
+          (TypingError
+             (parsed.parsed_pos, "Incompatible types for equality operator"))
   | PEQ | PNEQ -> (
       match ((fst e1).ttype, (fst e2).ttype) with
       | TRef a, TRef b when a = b ->
           ({ typed_pos = parsed.parsed_pos; ttype = TBool }, Binop (bop, e1, e2))
-      | _ -> failwith "typecheck error")
+      | _ ->
+          raise
+            (TypingError
+               (parsed.parsed_pos, "Incompatible types for equality operator")))
   | And | Or ->
       if (fst e1).ttype = TBool && (fst e2).ttype = TBool then
         ({ typed_pos = parsed.parsed_pos; ttype = TBool }, Binop (bop, e1, e2))
-      else failwith "typecheck error"
+      else
+        raise
+          (TypingError (parsed.parsed_pos, "Binary operator expects two bools"))
   | Ass -> (
       (* This is a mess. I'll clean it up later *)
       match snd e1 with
@@ -183,41 +212,55 @@ and typecheck_bop exp constr environ =
           | Some (TRef a) when (fst e2).ttype = a ->
               ( { typed_pos = parsed.parsed_pos; ttype = TUnit },
                 Binop (bop, e1, e2) )
-          | _ -> failwith "typecheck error")
-      | _ -> failwith "typecheck error")
+          | _ ->
+              raise
+                (TypingError
+                   (parsed.parsed_pos, "Incompatible types for assign operator"))
+          )
+      | _ ->
+          raise (TypingError (parsed.parsed_pos, "Assign operator expects var"))
+      )
   | Cat ->
       if (fst e1).ttype = TString && (fst e2).ttype = TString then
         ({ typed_pos = parsed.parsed_pos; ttype = TString }, Binop (bop, e1, e2))
-      else failwith "typecheck error"
+      else
+        raise
+          (TypingError (parsed.parsed_pos, "Cat operator expects two strings"))
   | Pipe -> (
       match ((fst e1).ttype, (fst e2).ttype) with
       | t, TFun (arg_t, e_t) when arg_t = t ->
           ({ typed_pos = parsed.parsed_pos; ttype = e_t }, App (e1, e2))
-      | _ -> failwith "argument type does not match function")
+      | _ ->
+          raise
+            (TypingError
+               (parsed.parsed_pos, "argument type does not match function")))
 
 and typecheck_unop exp constr environ =
   let prsed = fst exp in
   let uop, e =
     match exp with
     | _, Unaop (op, ex) -> (op, typecheck_expr ex constr environ)
-    | _ -> failwith "typecheck error"
+    | _ -> raise (TypingError (prsed.parsed_pos, "Expected unary operator"))
   in
   match uop with
   | Not ->
       if (fst e).ttype = TBool then
         ({ typed_pos = prsed.parsed_pos; ttype = TBool }, Unaop (uop, e))
-      else failwith "typecheck error"
+      else raise (TypingError (prsed.parsed_pos, "Not operator expected bool"))
   | Neg ->
       if (fst e).ttype = TInt then
         ({ typed_pos = prsed.parsed_pos; ttype = TInt }, Unaop (uop, e))
-      else failwith "typecheck error"
+      else raise (TypingError (prsed.parsed_pos, "Neg operator expected int"))
   | Ref ->
       ( { typed_pos = prsed.parsed_pos; ttype = TRef (fst e).ttype },
         Unaop (uop, e) )
   | Deref -> (
       match (fst e).ttype with
       | TRef a -> ({ typed_pos = prsed.parsed_pos; ttype = a }, Unaop (uop, e))
-      | _ -> failwith "typecheck error")
+      | _ ->
+          raise
+            (TypingError
+               (prsed.parsed_pos, "Dereference operator expected reference")))
 
 and parsed_to_typed_pat pat =
   match pat with
@@ -263,5 +306,4 @@ and parsed_to_typed_pat pat =
    | Constraint of (string * 'a expr_ann)
    | Constructor of (string * 'a expr_ann)
    | Record of (string * 'a expr_ann) list
-
    and unop = Not | Neg | Ref | Deref*)
